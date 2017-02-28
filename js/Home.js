30bab4ba80f337e704990225ca51688283d93cd4
itggot-John-Karlsson <john.karlsson@itggot.se>
2017-02-28 13:26:59 +0100

    Hej

---
+++ js/Home.js
@@ -0,0 +1,34 @@
+function hello() {
+	var icon1 = document.querySelector('.nav1');
+	var icon2 = document.querySelector('.nav2');
+	var content = document.querySelector('.content');
+	if (content.classList.contains('sidenav_sit1')) {
+		content.classList.remove('sidenav_sit1');
+		content.classList.add('sidenav_sit2');
+		icon1.classList.toggle('visible');
+		icon2.classList.toggle('visible');
+
+	} else {
+	content.classList.toggle('sidenav_sit2');
+	content.classList.toggle('sidenav_sit3');
+	icon1.classList.toggle('visible');
+	icon2.classList.toggle('visible');
+	}
+	var darken = document.querySelector('.ruski');
+	darken.classList.toggle('darken');
+}
+
+function helloguys () {
+	var icon1 = document.querySelector('.nav1');
+	var icon2 = document.querySelector('.nav2');
+	var content = document.querySelector('.content');
+	var darken = document.querySelector('.ruski');
+	if (darken.classList.contains('darken')) {
+		content.classList.toggle('sidenav_sit2');
+		content.classList.toggle('sidenav_sit3');
+		darken.classList.toggle('darken');
+		icon1.classList.toggle('visible');
+		icon2.classList.toggle('visible');
+	}
+}
+
